85d7ee3447ca33444bbc7fba335a8249d5693b2b
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-16 21:06:20 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Ejercicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Ejercicio
+    {
+        // a) Crea dos hilos compitiendo que traten de incrementar (el primer hilo) o decrementar (el segundo hilo) en 1 unidad una variable que comienza en 0.
+        // Funcionarán de forma continua hasta que a llegue a 1000 (primer hilo) o a -1000 (segundo hilo).
+        // Además se mostrará en pantalla la variable a cada vez que cambie indicando quién la ha cambiado(thread 1 o thread 2).
+        // Ambos hilos deben parar en cuanto uno consiga su objetivo.
+
+        //b) Las funciones de hilos serán expresiones lambda (si quieres y los ves claro haz ya directamente este apartado).
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            int num = 0; // Creo y declaro la variable que los hilos modificarán
+
+        }
+    }
+}
